85854657370a02d4e379508414a671513cd1ecfb
agustin <agustin@42mate.com>
2015-03-20 16:23:24 -0300

    Improvements in map

--- maps/js/map.js
+++ maps/js/map.js
@@ -2,31 +2,59 @@
  * Created by agustin on 3/19/15.
  */
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map;
+var layerControl;
+var points = [];
+var layers = [];
 
-var plotlayers=[];
+function busLines() {
+    var gpxLines = [
+        {
+            "url": "./data/linea-104.gpx",
+            "name": "Linea 104",
+            "colorLine": "red"
+        }
+    ];
+    var layer;
+    var name;
 
-// create the tile layer with correct attribution
-var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 11, maxZoom: 15, attribution: osmAttrib});
-
-
-var plotlist = [{"name":"Tunbridge Wells, Langton Road, Burnt Cottage",
-    "lon":"-27.4511985779",
-    "lat":"-58.9865379334",
-    "details":"A Grade II listed five bedroom wing in need of renovation."}];
-
-for (i=0;i<plotlist.length;i++) {
-    var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
-    var plotmark = new L.Marker(plotll);
-
-    plotmark.data=plotlist[i];
-    map.addLayer(plotmark);
-    plotmark.bindPopup("<h3>"+plotlist[i].name+"</h3>"+plotlist[i].details);
-    plotlayers.push(plotmark);
+    $.each(gpxLines, function(i, gpx) {
+        layer = new L.GPX(gpx.url, {
+            "async": false,
+            "marker_options": {
+                "startIconUrl": "./img/no-icon.png",
+                "endIconUrl": "./img/no-icon.png",
+                "shadowUrl": "./img/no-icon.png"
+            },
+            "polyline_options": {
+                "color": gpx.colorLine
+            }
+        });
+        name = "<img src='./img/" + gpx.colorLine + "-line.png' /> <span>" + gpx.name + "</span>";
+        layerControl.addOverlay(layer, name);
+    });
 }
 
-// start the map in South-East England
-map.setView(new L.LatLng(-27.4511985779, -58.9865379334), 13);
-map.addLayer(osm);
+$(document).ready(function () {
+    map = L.map('map',  {
+            fullscreenControl: true
+        }).setView([51.505, -0.09], 13);
+
+    // create the tile layer with correct attribution
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 11, maxZoom: 17, attribution: osmAttrib});
+
+    // start the map in South-East England
+    map.setView(new L.LatLng(-27.451389, -58.986667), 13);
+    map.addLayer(osm);
+
+    map.on('overlayadd', function(e) {
+        map.fitBounds(e.layer);
+    });
+
+    layerControl = L.control.layers(null, null, {collapsed: false});
+    layerControl.addTo(map);
+
+    busLines();
+});
